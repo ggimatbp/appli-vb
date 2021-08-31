6e71fb2385358587321593dd1c5221a75f7f3d14
gaetan <ggimat@batteries-prod.fr>
2021-08-31 11:22:40 +0200

    base du projet symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home/")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home/index.html.twig');
+    }
+}
