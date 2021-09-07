82712df7bfca494686b012dcda9ce9908dae8a02
gaetan <ggimat@batteries-prod.fr>
2021-09-07 09:38:32 +0200

    version propre avec user login form et log out et path user fixter

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use app\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+
+       private $passwordHasher;
+
+        public function __construct(UserPasswordHasherInterface $passwordHasher)
+        {
+            $this->passwordHasher = $passwordHasher;
+         }
+
+    public function load(ObjectManager $manager)
+    {
+    // Création d’un utilisateur de type “contributeur” (= auteur)
+    $contributor = new User();
+    $contributor->setEmail('contributor@monsite.com');
+    $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+    $contributor->setPassword($this->passwordHasher->hashPassword(
+        $contributor,
+        '123'
+    ));
+
+    $manager->persist($contributor);
+
+    // Création d’un utilisateur de type “administrateur”
+    $admin = new User();
+    $admin->setEmail('admin@monsite.com');
+    $admin->setRoles(['ROLE_ADMIN']);
+    $admin->setPassword($this->passwordHasher->hashPassword(
+        $admin,
+        '123'
+    ));
+
+    $manager->persist($admin);
+
+    // Sauvegarde des 2 nouveaux utilisateurs :
+    $manager->flush();
+        $manager->flush();
+    }
+}
