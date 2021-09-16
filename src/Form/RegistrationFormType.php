9963455bb7095a9b58c52834ddd774ae45c01aa8
gaetan <ggimat@batteries-prod.fr>
2021-09-16 17:04:42 +0200

    create user + role logic en lien direct avec ap role

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -43,6 +44,8 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('roleId', null, ['choice_label'=>'name'])
+
         ;
     }
 
