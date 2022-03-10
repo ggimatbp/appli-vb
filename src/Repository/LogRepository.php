790fc0107efe0fe2afa8a1697e809da1bb2aa10b
gaetan <ggimat@batteries-prod.fr>
2022-03-10 14:26:23 +0100

    abort logger but all logic is comments to save it

--- src/Repository/LogRepository.php
+++ src/Repository/LogRepository.php
@@ -1,23 +1,24 @@
 <?php
 
-namespace App\Repository;
+//part of the logging symfony see log.php/dBProcessor.php/DbHandler.php/DoctrineSubcriber.php/monolog.yaml/service.yaml
+// namespace App\Repository;
 
-use App\Entity\Log;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+// use App\Entity\Log;
+// use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+// use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Log|null find($id, $lockMode = null, $lockVersion = null)
- * @method Log|null findOneBy(array $criteria, array $orderBy = null)
- * @method Log[]    findAll()
- * @method Log[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class LogRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Log::class);
-    }
+// /**
+//  * @method Log|null find($id, $lockMode = null, $lockVersion = null)
+//  * @method Log|null findOneBy(array $criteria, array $orderBy = null)
+//  * @method Log[]    findAll()
+//  * @method Log[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+//  */
+// class LogRepository extends ServiceEntityRepository
+// {
+//     public function __construct(ManagerRegistry $registry)
+//     {
+//         parent::__construct($registry, Log::class);
+//     }
 
     // /**
     //  * @return Log[] Returns an array of Log objects
@@ -47,4 +48,4 @@ class LogRepository extends ServiceEntityRepository
         ;
     }
     */
-}
+//}
